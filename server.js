02efe5858abdd4063096cde6981300e8bf2faa29
HexagonalSole1 <deskrun@gmail.com>
2025-05-13 11:32:35 -0600

    change file server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Yo ese' });
+  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Ya no soy ese' });
 });
 
 const PORT = process.env.PORT || 3000;
